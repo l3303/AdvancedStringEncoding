d15f31edbd1ea6ea04c1874675424806852536da
xc_liu <xc_liu@Ctrip.com>
2018-01-08 18:47:29 +0800

    Implement VariableValue's hashCode

--- src/main/java/com/ken/VariableValue.java
+++ src/main/java/com/ken/VariableValue.java
@@ -7,12 +7,27 @@ public class VariableValue {
 
     private final long[] data;
 
+    /**
+     * Cache hashCode to speed up instance as key in hashMap
+     */
+    private final int hashCode;
+
     public VariableValue(long[] data) {
         this.data = data;
+        hashCode = calculateHashCode();
+    }
+
+    private int calculateHashCode() {
+        int result = 0;
+        for (long l : data) {
+            int tmp = (int)(l ^ (l >>> 32));
+            result = (((result << 5) + result) ^ tmp);
+        }
+        return result;
     }
 
     @Override
     public int hashCode() {
-        return 0;
+        return hashCode;
     }
 }
